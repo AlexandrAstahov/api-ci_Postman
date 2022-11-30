ef61cea3805c2113a0713239a9868861bb475a5d
Astahov_AYu <spirtt1985@yandex.ru>
2022-11-30 10:46:16 +0300

    Изменение теста, тест пройдет.

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("value"))
+                .body("data", equalTo("Школа Нетология"))
         ;
     }
 }
